aab2f91b1945ed8b13894b17d995f7a0f1cd4c04
Ameet Chaubal <ameet.chaubal@gmail.com>
2018-09-04 21:26:43 -0400

    added image

--- InputAnalysis.py
+++ InputAnalysis.py
@@ -38,29 +38,44 @@ def plot_variables():
     plt.show()
 
 
-def set_x_format(ax):
-    ax.tick_params(axis='both', labelsize=8)
-    l = ax.set_xticklabels(ind, rotation=45)
-
+def set_x_format(ax_array, data_array, title_array):
+    for i in range(len(ax_array)):
+        ax = ax_array[i]
+        title=title_array[i]
+        ax.tick_params(axis='both', labelsize=8)
+        l = ax.set_xticklabels(ind, rotation=45)
+        Xset = data_array[i]
+        ax.scatter(Xset[:,0],Xset[:,1], color=np.where(y_full > 0, 'r', 'b'))
+        ax.set_title(title, fontsize=12)
 
 # plot_variables()
 ind = np.arange(X.shape[0])
 fig, ax = plt.subplots(1, 3, figsize=(12, 4))
 fig.suptitle("Var-0 vs. Var-1\n", fontsize=12)
+title_array=["Unscaled data","After standard scaling (zoomed in)","After robust scaling (zoomed in)"]
+set_x_format(ax,[X,X1,X2],title_array)
 
-ax[0].scatter(X[:, 0], X[:, 1], color=np.where(y_full > 0, 'r', 'b'))
-ax[1].scatter(X1[:, 0], X1[:, 1], color=np.where(y_full > 0, 'r', 'b'))
-ax[2].scatter(X2[:, 0], X2[:, 1], color=np.where(y_full > 0, 'r', 'b'))
-for a in ax:
-    set_x_format(a)
-ax[0].set_title("Unscaled data", fontsize=12)
-ax[1].set_title("After standard scaling (zoomed in)", fontsize=12)
-ax[2].set_title("After robust scaling (zoomed in)", fontsize=12)
+# ax[0].scatter(X[:, 0], X[:, 1], color=np.where(y_full > 0, 'r', 'b'))
+# ax[1].scatter(X1[:, 0], X1[:, 1], color=np.where(y_full > 0, 'r', 'b'))
+# ax[2].scatter(X2[:, 0], X2[:, 1], color=np.where(y_full > 0, 'r', 'b'))
+# for a in ax:
+#     set_x_format(a)
+# ax[0].set_title("Unscaled data", fontsize=12)
+# ax[1].set_title("After standard scaling (zoomed in)", fontsize=12)
+# ax[2].set_title("After robust scaling (zoomed in)", fontsize=12)
 
 # for the scaled data, we zoom in to the data center (outlier can't be seen!)
 for a in ax[1:]:
     a.set_xlim(-3, 3)
     a.set_ylim(-3, 3)
 plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-fig.savefig("image\eyeState_v0_v1.png")
+
+def save_img(fig):
+    import os,sys
+    my_path = os.path.dirname(sys.argv[0])
+    my_file="eyeState_v0_V1.png"
+    my_dir = os.path.join(my_path,"image")
+    fig.savefig(os.path.join(my_dir,my_file))
+
+save_img(fig)
 plt.show()
